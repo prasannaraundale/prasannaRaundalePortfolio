901952f99dcb3685ee119b1ae4ac8091519f5c8d
prasanna <prasannaraundale23@gmail.com>
2024-03-01 12:51:28 +0530

    edit

--- force-app/main/default/lwc/clockDropdown/clockDropdown.js
+++ force-app/main/default/lwc/clockDropdown/clockDropdown.js
@@ -1,7 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ClockDropdown extends LightningElement {
-    label=''
-    option=[]
-    uniqueId=''
+    @api label=''
+    @api options=[]
+    @api uniqueId=''
 }
